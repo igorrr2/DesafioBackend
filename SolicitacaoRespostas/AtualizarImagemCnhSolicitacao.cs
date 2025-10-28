22bfbf8cb415898d77b40a4abf51200d251f899d
igorrr2 <igorrighi@hotmail.com>
2025-10-27 22:02:53 -0300

    Complemento commit

---
+++ SolicitacaoRespostas/AtualizarImagemCnhSolicitacao.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DesafioBackend.SolicitacaoRespostas
+{
+    public class AtualizarImagemCnhSolicitacao
+    {
+        [JsonPropertyName("imagem_cnh")]
+        public string ImagemCnh { get; set; } = string.Empty;
+    }
+}
